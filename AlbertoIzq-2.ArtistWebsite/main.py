8dc1ba55d78fd9fd9e9b021ac2ff5554f1a5ab15
Alberto <alberto.izquierdomo@gmail.com>
2020-10-25 11:31:08 +0100

    Add homeMobile.html to test and adapt website for mobile devices

--- AlbertoIzq-2.ArtistWebsite/main.py
+++ AlbertoIzq-2.ArtistWebsite/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__) # __name__ gets the .py name
 # Home page
 @app.route('/') # Declarator
 def home(): # Content
-    return render_template("home.html")
+    return render_template("homeMobile.html")
 
 if __name__ == "__main__": # It's True if this script is executed and not imported to another script
     app.run(debug = True)
